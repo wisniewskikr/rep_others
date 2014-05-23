590b1f299775baf94c4f5492dd62d7b0ddc28243
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2014-05-23 10:06:57 +0200

    Add templates handling to project OtherXmlEditor

--- OtherXmlEditor/src/main/java/org/wk/panels/TemplateXmlPanel.java
+++ OtherXmlEditor/src/main/java/org/wk/panels/TemplateXmlPanel.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -17,16 +20,11 @@ import javax.swing.JTree;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.wk.panels.SaveXmlPanel.SaveFileListener;
 import org.wk.services.CsvService;
 import org.wk.services.TreeService;
+import org.wk.services.VelocityService;
 import org.wk.swing.abstrs.AbstrPanel;
 
 
@@ -35,25 +33,30 @@ public class TemplateXmlPanel extends AbstrPanel{
 
 	private static final long serialVersionUID = 1L;
 	private JTree tree;	
-	private JTextField xmlFileTextField;
-	private JPanel xmlFileChoosePanel;
-	private File xmlFile;
+	private JTextField vmFileTextField;
+	private JPanel vmFileChoosePanel;
+	private File vmFile;
 	private JTextField csvFileTextField;
 	private JPanel csvFileChoosePanel;
 	private File csvFile;
 	private TreeService treeService;
 	private CsvService csvService;
+	private VelocityService velocityService;
+	
+	public final static String XML_FILE_PATH = System.getProperty("java.io.tmpdir") + File.separator;
+	public final static String XML_FILE_NAME = "tmp.xml";
 	
 	
 	public TemplateXmlPanel(JFrame frame){
 		super(frame);
 		this.treeService = new TreeService();
 		this.csvService = new CsvService();
+		this.velocityService = new VelocityService();
 		
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.add(getTitlePanel(), BorderLayout.NORTH);
 		this.add(getMessagePanel(), BorderLayout.NORTH);
-		this.add(getChooseXmlFilePanel(), BorderLayout.SOUTH);
+		this.add(getChooseVmFilePanel(), BorderLayout.SOUTH);
 		this.add(getChooseCsvFilePanel(), BorderLayout.SOUTH);
 		this.add(getButtonFillTemplatePanel(), BorderLayout.SOUTH);
 		this.add(getButtonPanel(), BorderLayout.SOUTH);
@@ -71,19 +74,19 @@ public class TemplateXmlPanel extends AbstrPanel{
 		
 	}
 	
-	private JPanel getChooseXmlFilePanel(){
+	private JPanel getChooseVmFilePanel(){
 		
-		xmlFileChoosePanel = new JPanel();
+		vmFileChoosePanel = new JPanel();
 		
-		xmlFileTextField = new JTextField(30);
-		xmlFileTextField.setEnabled(false);
-		xmlFileChoosePanel.add(xmlFileTextField);
+		vmFileTextField = new JTextField(30);
+		vmFileTextField.setEnabled(false);
+		vmFileChoosePanel.add(vmFileTextField);
 		
-		JButton button = new JButton("Choose Xml File ...");
-		button.addActionListener(new ChooseXmlFileListener());
-		xmlFileChoosePanel.add(button);
+		JButton button = new JButton("Choose Vm File ...");
+		button.addActionListener(new ChooseVmFileListener());
+		vmFileChoosePanel.add(button);
 		
-		return xmlFileChoosePanel;
+		return vmFileChoosePanel;
 		
 	}
 	
@@ -107,7 +110,7 @@ public class TemplateXmlPanel extends AbstrPanel{
 		
 		panel = new JPanel();
 		JButton button = new JButton("Fill Template");
-		button.addActionListener(new SaveFileListener());
+		button.addActionListener(new FillTemplateListener());
 		panel.add(button);
 		return panel;
 		
@@ -132,17 +135,17 @@ public class TemplateXmlPanel extends AbstrPanel{
 		
 	}
 	
-	 class ChooseXmlFileListener implements ActionListener {
+	 class ChooseVmFileListener implements ActionListener {
 		    public void actionPerformed(ActionEvent e) {
 		      JFileChooser c = new JFileChooser();
-		      FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter("xml files (*.xml)", "xml");
+		      FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter("vm files (*.vm)", "vm");
 		      c.setFileFilter(xmlfilter);
 		      int rVal = c.showOpenDialog(TemplateXmlPanel.this);
 		      if (rVal == JFileChooser.APPROVE_OPTION) {
-		        xmlFile = new File(c.getCurrentDirectory().toString() + System.getProperty("file.separator") + c.getSelectedFile().getName());
-		        xmlFileTextField.setText(xmlFile.getAbsolutePath());
-		        xmlFileChoosePanel.validate();
-		        xmlFileChoosePanel.repaint();
+		        vmFile = new File(c.getCurrentDirectory().toString() + System.getProperty("file.separator") + c.getSelectedFile().getName());
+		        vmFileTextField.setText(vmFile.getAbsolutePath());
+		        vmFileChoosePanel.validate();
+		        vmFileChoosePanel.repaint();
 		        cleanMessage();
 		      }		     
 		    }
@@ -164,33 +167,30 @@ public class TemplateXmlPanel extends AbstrPanel{
 		    }
 		  }
 	 
-	 class SaveFileListener implements ActionListener {
+	 class FillTemplateListener implements ActionListener {
 		 @Override
 			public void actionPerformed(ActionEvent e) {
 				
-			 if(xmlFile == null) {
+			 if(vmFile == null) {
 					displayMessage("Xml file can not be empty");
 					return;
 				}
 			 	
 			 	if(csvFile == null) {
-					displayMessage("Csv file can not be empty");
+					displayMessage("Vm file can not be empty");
 					return;
 				}  
 				
 				try {
 					
-					String[] headers = csvService.readHeaderFromFile(csvFile);
-					for (String header : headers) {
-						System.out.println("---header: " + header);
-					}
-										
-					List<String[]> lines = csvService.readLinesFromFile(csvFile);
-					for (String[] values : lines) {
-						for (String value : values) {
-							System.out.println("---value: " + value);
-						}
-					}
+					File xmlFile = new File(XML_FILE_PATH + XML_FILE_NAME);
+					handleTemplate(xmlFile);
+					
+					DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			        Document doc = docBuilder.parse(xmlFile);
+			        
+			        tree = treeService.convertDocument2Tree(doc);
 										
 				} catch (Exception exc) {
 					displayMessage("Following error occurs: " + exc.getMessage());
@@ -198,6 +198,25 @@ public class TemplateXmlPanel extends AbstrPanel{
 				}
 				
 				displayMessage("Template filled succuessfully");
+		 }
+		 
+		 private void handleTemplate(File xmlFile) {
+			 
+			 List<Map<String, String>> valuesList = new ArrayList<Map<String, String>>();
+				
+				String[] headers = csvService.readHeaderFromFile(csvFile);
+				List<String[]> lines = csvService.readLinesFromFile(csvFile);
+									
+				for (String[] values : lines) {
+					Map<String, String> map = new HashMap<String, String>();
+					for (int i = 0; i < values.length; i++) {
+						map.put(headers[i], values[i]);
+					}
+					valuesList.add(map);
+				}				
+				
+				velocityService.transformTemplate(vmFile, xmlFile, valuesList);
+			 
 		 }
 	 }	 
 	 
