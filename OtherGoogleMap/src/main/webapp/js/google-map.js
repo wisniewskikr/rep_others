966b6545d027c41f3e4d995854d381d228e4c07a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2014-07-15 10:46:23 +0200

    OthersGoogleMap: add frame of geolocation

--- OtherGoogleMap/src/main/webapp/js/google-map.js
+++ OtherGoogleMap/src/main/webapp/js/google-map.js
@@ -1,16 +1,25 @@
 function loadMap() {
-	var latlng = new google.maps.LatLng(53.3669049, 14.603458);
+	
+	//var latlng = new google.maps.LatLng(53.3669049, 14.603458);
 	var myOptions = {
 	  zoom: 17,
-	  center: latlng,
+//	  center: latlng,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("map_container"),myOptions);
+	var map = new google.maps.Map(document.getElementById("map_container"), myOptions);
  
-    var marker = new google.maps.Marker({
-      position: latlng, 
-      map: map, 
-      title:"Google Map!"
-    }); 
- 
-  }
+	var address = 'Polanka 20B, Poznan';
+	var geocoder = new google.maps.Geocoder();
+	  geocoder.geocode( { 'address': address}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      map.setCenter(results[0].geometry.location);
+	      var marker = new google.maps.Marker({
+	          map: map,
+	          position: results[0].geometry.location
+	      });
+	    } else {
+	      alert('Geocode was not successful for the following reason: ' + status);
+	    } 
+	  });
+  
+ }
